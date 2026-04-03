6581a13355c8a61cc0d21a29206cf4fced0fea5d
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-04-03 14:46:42 +0400

    Create hash function

--- HashMap.js
+++ HashMap.js
@@ -3,5 +3,17 @@ class HashMap {
     this.capacity = initialCapacity;
     this.loadFactor = loadFactor;
     this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
   }
 }
